19606b83472e889ff575287d183bd6344f831250
Darell Hendry <1darellhendry@gmail.com>
2020-10-02 09:37:42 +0700

    feat: preventing multi-calling thread

--- app/src/main/java/id/ac/ui/cs/mobileProgramming/helloworld/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileProgramming/helloworld/MainActivity.java
@@ -2,12 +2,10 @@ package id.ac.ui.cs.mobileProgramming.helloworld;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -18,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private final String[] helloWorlds = {"Hello World!","Kumusta, Mundo!", "Halo Dunia!", "Hallo Wereld!", "Bonjour le monde!"};
     private int stateIndex = 0;
     private boolean running = true;
-    private final Handler handler = new Handler();
+    final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,57 +26,54 @@ public class MainActivity extends AppCompatActivity {
             stateIndex = savedInstanceState.getInt("stateIndex");
             running = savedInstanceState.getBoolean("running");
         }
-        runHelloWorld();
+        handler.post(runnable); // run thread
     }
-
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putBoolean("running", running);
         outState.putInt("stateIndex", stateIndex);
     }
-
-    public void onClickExit(View view) {
-        finish();
-        System.exit(0);
-    }
-
-    public void onClickNext(View view) {
-        Intent setIntent = new Intent(this, NextActivity.class);
-        startActivity(setIntent);
-    }
-
     @Override
     public void onBackPressed() {
         Log.d(TAG, "onBackPressed");
         Toast toast = Toast.makeText(this, "to exit press exit button!", Toast.LENGTH_SHORT);
         toast.show();
     }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnable); // remove thread
+    }
 
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            if (stateIndex == helloWorlds.length) stateIndex = 0;
+            Log.d(TAG, helloWorlds[stateIndex]);
+            updateUiView(helloWorlds[stateIndex]);
+            if (running) {
+                stateIndex++;
+            }
+            handler.postDelayed(this, 1000);
+        }
+    };
+    public void updateUiView(String text) {
+        final TextView timeView = (TextView) findViewById(R.id.textView);
+        timeView.setText(helloWorlds[stateIndex]);
+    }
+    public void onClickExit(View view) {
+        finish();
+        System.exit(0);
+    }
+    public void onClickNext(View view) {
+        Intent setIntent = new Intent(this, NextActivity.class);
+        startActivity(setIntent);
+    }
     public void onClickResume(View view) {
         running = true;
     }
-
     public void onClickPause(View view) {
         running = false;
-    }
-
-    private void runHelloWorld() {
-        final TextView timeView = (TextView) findViewById(R.id.textView);
-
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                // Moves the current Thread into the background
-                android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
-                if (stateIndex == helloWorlds.length) stateIndex = 0;
-                Log.d(TAG, helloWorlds[stateIndex]);
-                timeView.setText(helloWorlds[stateIndex]);
-                if (running) {
-                    stateIndex++;
-                }
-                handler.postDelayed(this, 1000);
-            }
-        });
     }
 }

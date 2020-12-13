b0ec0ada1e7342742919f2a9b875d3fb6317b4ec
Darell Hendry <1darellhendry@gmail.com>
2020-12-13 09:39:36 +0700

    feat: hello

--- app/src/main/java/id/ac/ui/cs/mobileProgramming/helloworld/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileProgramming/helloworld/MainActivity.java
@@ -9,24 +9,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText mNumber1;
-    EditText mNumber2;
-    TextView mOutput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
-
-    public void onAdd(View view) {
-        mNumber1 = findViewById(R.id.field1);
-        mNumber2 = findViewById(R.id.field2);
-
-        double total = Double.parseDouble(mNumber1.getText().toString()) + Double.parseDouble(mNumber2.getText().toString());
-
-        mOutput = findViewById(R.id.output);
-        mOutput.setText(String.valueOf(total));
     }
 }
